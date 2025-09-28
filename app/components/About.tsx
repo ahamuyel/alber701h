cf65b0b5465f6357d32827245076c1c26cdd1b09
Alber701H <alber701hamuyela@gmail.com>
2025-09-28 08:08:20 +0000

    curriculum added

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -21,10 +21,12 @@ export default function About() {
               projeto, sempre aberto a desafios e colaboração.
             </p>
             <Link
-              href="#contact"
+              href="https://drive.google.com/file/d/1zpn44be4zR-EPPLsfNnkknL6r8bRa7UE/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferer"
               className="cta-button inline-block px-6 py-3 my-10 bg-teal-600 text-white rounded hover:bg-coral-500 transition-colors"
             >
-              Entre em Contato
+              Ver o meu currivullum
             </Link>
           </div>
 
@@ -32,7 +34,7 @@ export default function About() {
             <ul className="list-none space-y-4">
               <li className="flex items-start">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 mr-2 flex-shrink-0">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                  <path strokeLinecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
                 </svg>
                 <p>
                   Formação intensiva na <span>42 Luanda</span> com foco em <span>Shell, C, C++</span>, POO, algoritmos e boas práticas de engenharia de software.
